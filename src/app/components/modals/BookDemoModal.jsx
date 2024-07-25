82fcc7f19603a00753029c7512f739588108a963
Goloviychuk Vadim <vadim.g@twsgo.com>
2024-07-25 19:01:02 +0300

    added the updates in the book demo form

--- src/app/components/modals/BookDemoModal.jsx
+++ src/app/components/modals/BookDemoModal.jsx
@@ -1,46 +1,43 @@
-'use client';
-import { useState } from 'react';
-import Select from 'react-select'
+"use client";
+import { useState } from "react";
+import Select from "react-select";
 
 import { Button } from "@/app/components/Button";
 
-const url = 'https://www.movegenius.com/api/v1/enquireForDemo';
+const url = "https://onboarding-api-prd.azurewebsites.net/enquiries";
 
 const typeOptions = [
-  { value: 'partner', label: 'I am a partner (estate agent or conveyancer)' },
-  { value: 'home_mover', label: 'I am a home mover ' },
+  { value: "contactUs", label: "Contact Us" },
+  { value: "demo", label: "Demo" },
 ];
 
 const colourStyles = {
   control: (styles) => ({
     ...styles,
-    border: '1px solid #29262C',
-    borderRadius: '8px',
-    fontSize: '14px',
-    lineHeight: '20px',
-    fontWeight: '500'
+    border: "1px solid #29262C",
+    borderRadius: "8px",
+    fontSize: "14px",
+    lineHeight: "20px",
+    fontWeight: "500",
   }),
-  option: (styles) => (
-    {
-      ...styles,
-      fontSize: '14px',
-      lineHeight: '20px',
-      fontWeight: '500'
-    }
-  ),
+  option: (styles) => ({
+    ...styles,
+    fontSize: "14px",
+    lineHeight: "20px",
+    fontWeight: "500",
+  }),
 };
 
 const initialForm = {
-  name: '',
-  email: '',
-  type: typeOptions[0],
-  message: ''
+  name: "",
+  email: "",
+  enquiryType: typeOptions[0],
+  message: "",
 };
 
 const defaultErrors = {
-  name: '',
-  email: '',
-  submit: '',
+  email: "",
+  submit: "",
 };
 
 export const BookDemoModal = () => {
@@ -48,6 +45,7 @@ export const BookDemoModal = () => {
   const [formData, setFormData] = useState(initialForm);
   const [errors, setErrors] = useState(defaultErrors);
 
+  console.log("formData", formData);
   const handleChangeForm = (key, value) => {
     setFormData((prevState) => ({ ...prevState, [key]: value }));
   };
@@ -58,106 +56,132 @@ export const BookDemoModal = () => {
   };
 
   const handleChangeSelect = (opt) => {
-    handleChangeForm('type', opt);
+    handleChangeForm("enquiryType", opt);
   };
 
   const handleSubmit = async () => {
     setErrors(defaultErrors);
-    if (!formData.name) {
-      setErrors((prevState) => ({ ...prevState, name: 'This field is required!' }))
-    }
 
     if (!formData.email) {
-      setErrors((prevState) => ({ ...prevState, email: 'This field is required!' }))
+      setErrors((prevState) => ({
+        ...prevState,
+        email: "This field is required!",
+      }));
     }
 
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
-      setErrors((prevState) => ({ ...prevState, email: 'The format is wrong!' }))
+      setErrors((prevState) => ({
+        ...prevState,
+        email: "The format is wrong!",
+      }));
     }
 
-    if (!errors.email && !errors.name) {
+    if (!errors.email) {
       try {
         await fetch(url, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({ ...formData, type: formData.type.value }),
+          body: JSON.stringify({
+            ...formData,
+            enquiryType: formData.enquiryType.value,
+          }),
         });
         setFormData(initialForm);
         setShowModal(false);
       } catch (e) {
-        setErrors((prevState) => ({ ...prevState, submit: 'Sorry we couldn\'t send your enquiry, please try again later!' }))
+        setErrors((prevState) => ({
+          ...prevState,
+          submit:
+            "Sorry we couldn't send your enquiry, please try again later!",
+        }));
       }
     }
   };
 
   return (
     <>
-      <Button className="text-coral bg-white" onClick={() => setShowModal(true)}>Book a demo</Button>
+      <Button
+        className="text-coral bg-white"
+        onClick={() => setShowModal(true)}
+      >
+        Book a demo
+      </Button>
       {showModal && (
         <div
           className="fixed top-0 left-0 w-screen h-full flex items-center justify-center"
           style={{
             zIndex: 1,
-            background: 'rgba(0, 0, 0, 0.3)'
+            background: "rgba(0, 0, 0, 0.3)",
           }}
           onClick={() => setShowModal(false)}
         >
-          <div className="w-[500px] rounded-[24px] bg-white p-[30px]" onClick={(e) => {
-            e.stopPropagation()
-          }}>
+          <div
+            className="w-[500px] rounded-[24px] bg-white p-[30px]"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
             <div className="mb-[15px] flex flex-col gap-2">
-              <label
-                htmlFor="name"
-              >
-                <span className="text-xs font-medium">Name </span>
-                <span className="text-coral">*</span>
-                {errors.name && <span className="text-coral text-xs font-light"> {errors.name}</span>}
-              </label>
-              <input
-                id="name"
-                type="text"
-                className={`text-xs font-medium p-2 border rounded-[8px] ${errors.name ? 'border-coral' : 'border-charcoal'}`}
-                value={formData.name}
-                onChange={handleChangeTextInputs('name')}
-                placeholder="Enter your name"
-              />
-            </div>
-            <div className="mb-[15px] flex flex-col gap-2">
-              <label
-                htmlFor="email"
-              >
+              <label htmlFor="email">
                 <span className="text-xs font-medium">Email </span>
                 <span className="text-coral">*</span>
-                {errors.email && <span className="text-coral text-xs font-light"> {errors.email}</span>}
+                {errors.email && (
+                  <span className="text-coral text-xs font-light">
+                    {" "}
+                    {errors.email}
+                  </span>
+                )}
               </label>
               <input
                 id="email"
                 type="text"
-                className={`text-xs font-medium p-2 border rounded-[8px] ${errors.email ? 'border-coral' : 'border-charcoal'}`}
+                className={`text-xs font-medium p-2 border rounded-[8px] ${errors.email ? "border-coral" : "border-charcoal"}`}
                 value={formData.email}
-                onChange={handleChangeTextInputs('email')}
+                onChange={handleChangeTextInputs("email")}
                 placeholder="Enter your email"
               />
             </div>
             <div className="mb-[15px] flex flex-col gap-2">
-              <label
-                htmlFor="type"
-              >
-                <span className="text-xs font-medium">Type </span>
+              <label htmlFor="contactNumber">
+                <span className="text-xs font-medium">Phone number</span>
+              </label>
+              <input
+                id="contactNumber"
+                type="text"
+                className="text-xs font-medium p-2 border rounded-[8px] border-charcoal"
+                value={formData.contactNumber}
+                onChange={handleChangeTextInputs("contactNumber")}
+                placeholder="Enter your phone number"
+              />
+            </div>
+            <div className="mb-[15px] flex flex-col gap-2">
+              <label htmlFor="name">
+                <span className="text-xs font-medium">Name</span>
+              </label>
+              <input
+                id="name"
+                type="text"
+                className="text-xs font-medium p-2 border rounded-[8px] border-charcoal"
+                value={formData.name}
+                onChange={handleChangeTextInputs("name")}
+                placeholder="Enter your name"
+              />
+            </div>
+            <div className="mb-[15px] flex flex-col gap-2">
+              <label htmlFor="type">
+                <span className="text-xs font-medium">Type</span>
               </label>
               <Select
                 options={typeOptions}
-                value={formData.type}
+                value={formData.enquiryType}
                 onChange={handleChangeSelect}
                 styles={colourStyles}
               />
             </div>
             <div className="mb-[15px] flex flex-col gap-2">
-              <label
-                htmlFor="message"
-              >
+              <label htmlFor="message">
                 <span className="text-xs font-medium">Message</span>
               </label>
               <textarea
@@ -165,7 +189,7 @@ export const BookDemoModal = () => {
                 rows={4}
                 className="text-xs font-medium p-2 border border-charcoal rounded-[8px]"
                 value={formData.message}
-                onChange={handleChangeTextInputs('message')}
+                onChange={handleChangeTextInputs("message")}
                 placeholder="Enter your message"
               />
             </div>
@@ -178,7 +202,11 @@ export const BookDemoModal = () => {
                 Send
               </Button>
             </div>
-            {errors.submit && <p className="mt-[10px] text-xs text-coral font-medium text-center">{errors.submit}</p>}
+            {errors.submit && (
+              <p className="mt-[10px] text-xs text-coral font-medium text-center">
+                {errors.submit}
+              </p>
+            )}
           </div>
         </div>
       )}
